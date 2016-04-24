97ef29dcb2f032047536241266fcc0996cc20567
Tim Joosten <Tjoosten@users.noreply.github.com>
2016-04-24 19:41:19 +0200

    Fix #75 -> Add missing tests for the FAQ controller (#58)
    - Added missing routes to the controllers
    - Fix bug in request validation
    - Added attributes in the request validation
    - Added tests for the missing routes and controllers
    - Added view: resources/views/backend/faq/edit.blade.php
    - Added view: resources/views/backend/faq/insert.blade.php

--- tests/FaqTest.php
+++ tests/FaqTest.php
@@ -18,4 +18,110 @@ class FaqTest extends TestCase
     {
         $this->visit('/faq')->seeStatusCode(200);
     }
+    
+    /**
+     * GET: /faq/delete/{id}
+     *
+     * @group all
+     * @group faq
+     */
+    public function testFaqDestroy()
+    {
+        $user = factory(App\User::class)->create();
+        $item = factory(App\Faq::class)->create();
+
+        $this->actingAs($user)
+            ->seeIsAuthenticated()
+            ->seeInDatabase('faqs', ['question' => $item->question])
+            ->visit('faq/delete/' . $item->id)
+            ->notSeeInDatabase('faqs', ['question' => $item->question])
+            ->seeStatusCode(200);
+    }
+
+    /**
+     * GET: /faq/edit/{id}
+     *
+     * @group all
+     * @group faq
+     */
+    public function testEditView()
+    {
+        $user = factory(App\User::class)->create();
+        $item = factory(App\Faq::class)->create();
+
+        $this->actingAs($user)
+            ->seeIsAuthenticated()
+            ->visit('/faq/edit/' . $item->id)
+            ->seeStatusCode(200);
+    }
+
+    /**
+     * POST: /faq/edit/{id}
+     *
+     * @group all
+     * @group faq
+     */
+    public function testEditPostMethod()
+    {
+        $this->withoutMiddleware();
+        $user = factory(App\User::class)->create();
+        $item = factory(App\Faq::class)->create();
+
+        // Inputs
+        $input['answer']   = 'Answer';
+        $input['question'] = 'Question';
+
+        // Database
+        $oldDb['id']       = $item->id;
+        $oldDb['answer']   = $item->answer;
+        $oldDb['question'] = $item->question;
+
+        $newDb['id']       = $item->id;
+        $newDb['answer']   = $input['answer'];
+        $newDb['question'] = $input['question'];
+
+        $this->actingAs($user)
+            ->seeIsAuthenticated()
+            ->seeInDatabase('faqs', $oldDb)
+            ->post('/faq/edit/' . $item->id, $input)
+            ->dontSeeInDatabase('faqs', $oldDb)
+            ->seeInDatabase('faqs', $newDb)
+            ->seeStatusCode(302);
+    }
+
+    /**
+     * GET: /faq/create
+     *
+     * @group all
+     * @group faq
+     */
+    public function testCreateView()
+    {
+        $user = factory(App\User::class)->create();
+
+        $this->actingAs($user)
+            ->seeIsAuthenticated()
+            ->visit('faq/create')
+            ->seeStatusCode(200);
+    }
+
+    /**
+     * POST: /faq/create
+     *
+     * @group all
+     * @group faq
+     */
+    public function testStoreFaqMethod()
+    {
+        $user = factory(App\User::class)->create();
+
+        $input['question'] = 'Question';
+        $input['answer']   = 'Answer';
+
+        $this->actingAs($user)
+            ->seeIsAuthenticated()
+            ->post('faq/create', $input)
+            ->seeInDatabase('faqs', $input)
+            ->seeStatusCode(302);
+    }
 }
