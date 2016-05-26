2848cd8cbc3a185158e8fe2e085cd673ef41c75d
Tjoosten <Tjoosten3@gmail.com>
2016-05-26 21:12:25 +0200

    Update #3 -> add destroy method

--- app/Http/Controllers/LabelController.php
+++ app/Http/Controllers/LabelController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Labels;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -9,5 +10,44 @@ use App\Http\Controllers\Controller;
 
 class LabelController extends Controller
 {
-    //
+    /**
+     * LabelController constructor.
+     */
+    public function __construct()
+    {
+
+    }
+
+    public function create()
+    {
+
+    }
+
+    public function store()
+    {
+
+    }
+
+    public function edit($id)
+    {
+
+    }
+
+    public function update($id)
+    {
+
+    }
+
+    /**
+     * Destroy a label.
+     * 
+     * @param  int $id the database id off the label
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function destroy($id)
+    {
+        Labels::destroy($id);
+        session()->flash('message', 'Label deleted');
+        return redirect()->back(302);
+    }
 }
