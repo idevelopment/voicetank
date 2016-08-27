44ee07b48800a24b8342822082ce4050739a7a03
Tjoosten <Tjoosten3@gmail.com>
2016-08-27 23:33:41 +0200

    Add teams save controller

--- app/Http/Controllers/TeamsController.php
+++ app/Http/Controllers/TeamsController.php
@@ -2,11 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\TeamsValidator;
 use App\Teams;
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 
+/**
+ * Class TeamsController
+ * @package App\Http\Controllers
+ */
 class TeamsController extends Controller
 {
     /**
@@ -29,5 +33,22 @@ class TeamsController extends Controller
     {
         $data['teams'] = Teams::paginate(15);
         return view('teams.index', $data);
+    }
+
+    /**
+     * Save a new team in the application.
+     *
+     * @url:platform
+     * @see:phpunit
+     *
+     * @param  TeamsValidator $input
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function save(TeamsValidator $input)
+    {
+        session()->flash('class', 'alert alert-success');
+        session()->flash('message', 'The team has been added.');
+
+        return redirect()->back();
     }
 }
