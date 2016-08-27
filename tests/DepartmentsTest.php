de198b6b71e9f79ca21dabd34ef2e202e7c9e673
Tjoosten <Tjoosten3@gmail.com>
2016-08-28 00:15:41 +0200

    Add testing classes for Departments

--- tests/DepartmentsTest.php
+++ tests/DepartmentsTest.php
@@ -9,13 +9,71 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
  */
 class DepartmentsTest extends TestCase
 {
+    // DatabaseMigrations   = Used for the database migrations.
+    // DatabaseTransactions = Used fo all the database transactions.
+    use DatabaseMigrations, DatabaseTransactions;
+
     /**
-     * A basic test example.
+     * GET|HEAD: /users/departments
+     * ROUTE:    departments.index
      *
-     * @return void
+     * @group all
+     * @group departments
      */
-    public function testExample()
+    public function testOverviewTeams()
     {
-        $this->assertTrue(true);
+
+    }
+
+    /**
+     * GET|HEAD: /users/departments/{id}
+     * ROUTE:    departments.show
+     *
+     * @group all
+     * @group departments
+     */
+    public function testShowSpecificTeam()
+    {
+
+    }
+
+    /**
+     * POST:     /users/departments/saved
+     * ROUTE:    departments.save
+     *
+     * - Without validation errors
+     *
+     * @group all
+     * @group departments
+     */
+    public function testInsertTeamWithoutErrors()
+    {
+
+    }
+
+    /**
+     * POST:     /users/departments/saved
+     * ROUTE:    departments.save
+     *
+     * - With validation errors
+     *
+     * @group all
+     * @group departments
+     */
+    public function testInsertNewTeamWithErrors()
+    {
+
+    }
+
+    /**
+     * GET|HEAD: /users/departments/destroy/{id}
+     * ROUTE:    departments.destroy
+     *
+     * @group all
+     * @group departments
+     */
+    public function testDestroyingTeam()
+    {
+
     }
 }
