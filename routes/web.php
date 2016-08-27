f75a641be6d84d38976034443a2fdb5ba9ff0aa1
Tjoosten <Tjoosten3@gmail.com>
2016-08-27 23:20:22 +0200

    Add teams index route

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,8 @@ Route::get('/users/departments/{id}', 'DepartmentsController@show')->name('depar
 Route::get('/users/departments/destroy/{id}', 'DepartmentsController@destroy')->name('departments.destroy');
 
 
+Route::get('/users/teams', 'TeamsController@index')->name('teams.index');
+
 Route::get('/projects', 'ProjectsController@index')->name('projects.index');
 Route::get('/projects/details', 'ProjectsController@details')->name('projects.details');
 Route::get('/projects/create', 'ProjectsController@register')->name('projects.register');
