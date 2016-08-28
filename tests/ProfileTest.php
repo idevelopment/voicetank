5a572aa4e4183bd20912d3c777dfc8c5573d7d18
Tjoosten <Tjoosten3@gmail.com>
2016-08-28 06:14:17 +0200

    Update test

--- tests/ProfileTest.php
+++ tests/ProfileTest.php
@@ -9,11 +9,34 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
  *
  * // TODO: write the tests
  * // TODO: write the docblocks
- * // TODO: document traits.
  */
 class ProfileTest extends TestCase
 {
+    // DatabaseMigrations   = Used for the database migrations.
+    // DatabaseTransactions = Used fo all the database transactions.
     use DatabaseMigrations, DatabaseTransactions;
+
+    /** @var array $user Database users */
+    protected $user;
+
+    /**
+     * Set up all the needed factories.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        // Database transactions factories.
+        $this->user = factory(App\User::class)->create();
+    }
+    /**
+     * Authencation class -> Used for test authencated routes.
+     */
+    protected function authencation()
+    {
+        $this->actingAs($this->user);
+        $this->seeIsAuthenticatedAs($this->user);
+    }
 
     /**
      * GET|HEAD: /profile
@@ -24,61 +47,86 @@ class ProfileTest extends TestCase
      */
     public function testProfileSettingsView()
     {
-
+        $this->withoutMiddleware();
+        $this->authencation();
     }
 
     /**
+     * POST:
+     * ROUTE:
+     *
      * @group all
      * @group profile
      */
     public function testUpdateInfoWithErrors()
     {
-        //
+        $this->withoutMiddleware();
+        $this->authencation();
     }
 
     /**
+     * POST:
+     * ROUTE:
+     *
      * @group all
      * @group profile
      */
     public function testUpdateInfoWithoutErrors()
     {
-        //
+        $this->withoutMiddleware();
+        $this->authencation();
     }
 
     /**
+     * POST:
+     * ROUTE:
+     *
      * @group all
      * @group profile
      */
     public function testUpdatePasswordWithErrors()
     {
-        //
+        $this->withoutMiddleware();
+        $this->authencation();
     }
 
     /**
+     * POST:
+     * ROUTE:
+     *
      * @group all
      * @group profile
      */
     public function testUpdatePasswordWithoutErrors()
     {
-        //
+        $this->withoutMiddleware();
+        $this->authencation();
     }
 
 
     /**
+     * POST:
+     * ROUTE:
+     *
      * @group all
      * @group profile
      */
     public function testUpdateContactInfoWithErrors()
     {
-        //
+        $this->withoutMiddleware();
+        $this->authencation();
     }
 
     /**
+     * POST:
+     * ROUTE:
+     *
      * @group all
      * @group profile
      */
     public function testUpdateContactInfoWithoutErrors()
     {
-        //
+        $this->withoutMiddleware();
+        $this->authencation();
     }
 }
