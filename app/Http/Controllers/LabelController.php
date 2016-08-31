0842a2c03c48eac0f544abd2a5f0d0995762b5db
Tim Joosten <Tjoosten3@gmail.com>
2016-08-31 17:45:39 +0200

    Controller update

--- app/Http/Controllers/LabelController.php
+++ app/Http/Controllers/LabelController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use PhpParser\Node\Stmt\Label;
 
 /**
  * Class LabelController
@@ -37,31 +38,49 @@ class LabelController extends Controller
      */
     public function store()
     {
+        session()->flash('', '');
+        session()->flash('', '');
 
+        return redirect()->back();
     }
 
+    /**
+     * Display a specific label in the application.
+     *
+     * @param  int $id the label id in the database.
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function show($id)
     {
-
+        return view('')
     }
 
     /**
      * Update a label in the database.
      *
-     * @param int $id the id for the label in the database.
+     * @param  Requests\LabelValidator $input
+     * @param  int $id the id for the label in the database.
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function update($id)
+    public function update(Requests\LabelValidator $input, $id)
     {
+        session()->flash('', '');
+        session()->flash('', '');
 
+        return redirect()->back();
     }
 
     /**
      * Destroy a label out off the database.
      *
-     * @param int $id the id for the label in the database.
+     * @param  int $id the id for the label in the database.
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function destroy($id)
     {
+        session()->flash('', '');
+        session()->flash('', '');
 
+        return redirect()->back();
     }
 }
