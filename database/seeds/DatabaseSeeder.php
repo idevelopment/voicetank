96a20a4d6670732c57c53ccffab34f97105fb9ef
Tim Joosten <Tjoosten3@gmail.com>
2016-09-08 23:05:29 +0200

    Add category seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UserTableSeeder::class);
         $this->call(Countries::class);
         $this->call(RolesTableSeeder::class);
+        $this->call(CategoryTableSeeder::class);
     }
 }
